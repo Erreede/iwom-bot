2b022ecc8d1bd1c018acf4e47df778c42bdd008d
Pablo Bañuelos <pablo.banuelos@dxc.com>
2022-12-16 20:38:59 +0100

    Added some explanations

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import os
 
 # Rellenar el archivo 'holidays.json' con los días de vacaciones y el archivo 'others.json' con una equivalencia a la siguiente matriz:
-# 01 - Vacaciones -> 14
 # 02 - Baja médica -> 15
 # 03 - Maternidad / paternidad -> 16
 # 04 - Permiso retribuido -> 17
@@ -12,6 +11,8 @@ import os
 # 07 - Descanso semanal (trunos) -> 20
 # 08 - Compensación de horas -> 21
 # 09 - Huelga -> 9
+
+#Ejecutar el script como está para registrar el día en curso, cambiar la linea comentada para registrar desde una fecha
 
 if __name__ == "__main__":
     reg = iWom(os.getenv('username'), os.getenv('password'), datetime.now().strftime('%d/%m/%Y'))
